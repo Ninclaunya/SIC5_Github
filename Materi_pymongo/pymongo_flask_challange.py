ad97d9162d4d3611713307a522f81f7215f01bff
Ninclaunya <nindie0304@gmail.com>
2024-07-13 13:13:16 +0700

    Update pymongo_flask_challange.py

--- Materi_pymongo/pymongo_flask_challange.py
+++ Materi_pymongo/pymongo_flask_challange.py
@@ -1,30 +1,39 @@
 import pymongo
 from flask import Flask, request
+from pymongo.results import InsertManyResult
 
 app = Flask(__name__)
 
-password = "Angkasa1"
-uri: LiteralString = f"mongodb+srv://Ninclaunya:<password>@sicpractice.1dr8jgg.mongodb.net/?retryWrites=true&w=majority&appName=SICPractice"
+# password = 'ebNcWXOU9EiNrSJq'
+uri = "mongodb+srv://Ninclaunya:Angkasa1@sicpractice.1dr8jgg.mongodb.net/?retryWrites=true&w=majority&appName=SICPractice"
 
 client = pymongo.MongoClient(uri)
-db: Database[Any] = client.challange1
-myCollection: Collection[Any] = db.sensor
+db = client.challenge1
+myCollection = db.sensor
 
-@app.route("/", methods= ["GET"])
+def kirim_data(temperature, kelembapan):
+    sensor = {'kelembapan':kelembapan, 'temperature':temperature}
+    results = myCollection.insert_many([sensor])
+    print(results.inserted_ids)
+
+@app.route('/', methods= ["GET"])
 def hello_world():
-    print("Hello World")
-    
-@app.route("/sensor1", methods= ["POST"])
-def sensor1():
-    temperature: str | None = request.argst.get("temperature")
-    humidity: str | None = request.args.get("kelembapan")
-    
+    return "Hello World"
+
+@app.route('/sensor1', methods= ["POST"]) # type: ignore
+def data():
+    temperature = request.args.get("temperature")
+    kelembapan = request.args.get("kelembapan")
+
     if temperature is not None:
         temperature = float(temperature)
-    if humidity is not None:
-        humidity = float(humidity)
-        
-    sensor: dict[float | None, float | ...]
+    if kelembapan is not None:
+        kelembapan = float(kelembapan)
+
+    #mengirim data sensor ke db
+    kirim_data(temperature=temperature, kelembapan=kelembapan)
+
+    return f'temperature: {temperature}, kelembapan: {kelembapan}'
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0",debug=True)
